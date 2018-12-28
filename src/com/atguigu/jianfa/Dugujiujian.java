563eb01b8e19ba5c468c6cbd0ad119f71daea1e2
songzz2013 <muzikou2013@163.com>
2018-12-28 11:45:38 +0800

    令狐冲提交1234

--- src/com/atguigu/jianfa/Dugujiujian.java
+++ src/com/atguigu/jianfa/Dugujiujian.java
@@ -3,7 +3,10 @@ package com.atguigu.jianfa;
 public class Dugujiujian {
 
 	public static void main(String[] args) {
-		System.out.println("独孤九剑");
+		System.out.println("独孤九剑1234");
+		System.out.println("独孤九剑1234");
+		System.out.println("独孤九剑1234");
+		System.out.println("独孤九剑1234");
 	}
 
 }
