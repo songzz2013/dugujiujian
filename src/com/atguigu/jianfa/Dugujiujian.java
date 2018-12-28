07858cf5daeb0c4bceeef5a3717ca55dd2d2a241
songzz2018 <muzikou2006@163.com>
2018-12-28 11:30:53 +0800

    岳不群提交

--- src/com/atguigu/jianfa/Dugujiujian.java
+++ src/com/atguigu/jianfa/Dugujiujian.java
@@ -4,6 +4,11 @@ public class Dugujiujian {
 
 	public static void main(String[] args) {
 		System.out.println("独孤九剑");
+		System.out.println("独孤九剑");
+		System.out.println("独孤九剑");
+		System.out.println("独孤九剑");
+		System.out.println("独孤九剑");
+		System.out.println("独孤九剑");
 	}
 
 }
